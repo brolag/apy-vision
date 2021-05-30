58baaaf96a847020c365b32bba7c3267a7ce3abb
Alfredo Bonilla <brolag@live.com>
2021-05-29 18:40:53 -0600

    fix: number classes interpolation

--- components/LPGains/index.tsx
+++ components/LPGains/index.tsx
@@ -7,13 +7,14 @@ import { getNumberColorClass } from "@utils/getNumberColor"
 
 export default function LPGains({ userPool }) {
   const numberColorClass = getNumberColorClass(userPool.netGainUsd)
+  const numberClasses = `${numberColorClass} font-bold text-sm`
 
   return (
     <div>
       <div className="font-bold">
         {applyCurrencyFormat(userPool.netGainUsd)}
       </div>
-      <div className={`${numberColorClass} font-bold text-sm`}>
+      <div className={numberClasses}>
         {applyGainsPctFormat(userPool.netGainPct)} (
         {applyGainsFormat(userPool.netGainUsd)})
       </div>
--- components/TokensInfo/index.tsx
+++ components/TokensInfo/index.tsx
@@ -7,13 +7,14 @@ export default function TokensInfo({ tokens }) {
       {tokens.map((token, index) => {
         const gridStart = `col-start-${index % 2 === 0 ? 1 : 2}`
         const numberColorClass = getNumberColorClass(token.tokenUsdGain)
+        const numberClasses = `${numberColorClass} font-bold text-sm`
         return (
           <div key={token.tokenName} className={`col-span-1 ${gridStart} mb-6`}>
             <div>
               {token.tokenStartingBalance.toFixed(2)} →{" "}
               {token.tokenCurrentBalance.toFixed(2)} {token.tokenName}
             </div>
-            <div className={`${numberColorClass} font-bold text-sm`}>
+            <div className={numberClasses}>
               {token.tokenUsdGain.toFixed(2)} {token.tokenName}{" "}
               {applyGainsFormat(token.tokenCurrentBalance)}
             </div>
